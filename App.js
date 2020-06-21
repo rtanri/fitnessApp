9cef0e420a21fde75c89c44c4d744fe8817976f5
Revin Tanri <tanrirevin@gmail.com>
2020-06-21 12:15:17 +0800

    Add /utils/ with calendar, colors, and helpers

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Ionicons} from  '@expo/vector-icons'
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 
 export default class App extends React.Component {
@@ -13,7 +14,9 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Welcome to React Native!</Text>
-        <Ionicons name='ios-pizza' color='blue' size={100}/>
+        <Ionicons name='ios-pizza' color='blue' size={80}/>
+        
+        <FontAwesome5 name="dumbbell" size={80} color="blue" />
 
       </View>
     );
