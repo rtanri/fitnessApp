603f4ff2a90142994e9912057907f9b3ad65efc0
Revin Tanri <tanrirevin@gmail.com>
2020-06-22 21:08:59 +0800

    Add TextButton, and Props.alreadyLogged

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import {Ionicons} from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }){
     return (
@@ -74,9 +76,31 @@ export default class AddEntry extends Component {
         //Clear local notification
     }
 
+    reset = () => {
+        const key = timeToString()
+        // Update Redux
+        // Route to home
+        // Update DB
+    }
+
     render(){
         const metaInfo = getMetricMetaInfo()
         
+        if (this.props.alreadyLogged){
+            return (
+                <View>
+                    <Ionicons 
+                        name="ios-happy" 
+                        size={24} 
+                        color="black" 
+                    />
+                    <Text>You already logged your information for today</Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
         return (
             <View>
                 <DateHeader date={(new Date()).toLocaleDateString()} />
